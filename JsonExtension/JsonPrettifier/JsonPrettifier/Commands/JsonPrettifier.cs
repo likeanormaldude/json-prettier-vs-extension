9ccd7924e9144e695ae38a915b614f4beb206607
Deivid Rodrigues <deivid.likeanormaldude@hotmail.com>
2025-07-11 17:51:26 -0400

    JSON Prettify extension for VS
    # Conflicts:
    #	.gitignore

---
+++ JsonExtension/JsonPrettifier/JsonPrettifier/Commands/JsonPrettifier.cs
@@ -0,0 +1,57 @@
+﻿using System.Linq;
+using EnvDTE;
+using EnvDTE80;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JsonPrettifier;
+
+[Command(PackageIds.MyCommand)]
+internal sealed class JsonPrettifier : BaseCommand<JsonPrettifier>
+{
+    protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+    {
+        await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+
+        DTE2 dte = (DTE2)ServiceProvider.GlobalProvider.GetService(typeof(DTE));
+        Document activeDoc = dte?.ActiveDocument;
+
+        if (activeDoc == null)
+        {
+            await VS.MessageBox.ShowAsync("No active document!");
+            return;
+        }
+
+        TextDocument textDoc = activeDoc.Object() as TextDocument;
+        EditPoint startPoint = textDoc?.StartPoint.CreateEditPoint();
+
+        if (textDoc == null || startPoint == null)
+        {
+            await VS.MessageBox.ShowAsync("Cannot access text in the current document.");
+            return;
+        }
+
+        string text = startPoint.GetText(textDoc.EndPoint);
+
+        try
+        {
+            var lines = text.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            var prettifiedLines = lines.Select(line =>
+            {
+                var parsed = JToken.Parse(line);
+                return parsed.ToString(Formatting.Indented);
+            });
+
+            string pretty = string.Join(Environment.NewLine, prettifiedLines);
+            startPoint.ReplaceText(textDoc.EndPoint, pretty, 0);
+        }
+        catch (JsonReaderException ex)
+        {
+            await VS.MessageBox.ShowAsync("Invalid JSON:\n" + ex.Message);
+        }
+        catch (Exception ex)
+        {
+            await VS.MessageBox.ShowAsync("Error:\n" + ex.Message);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# json-prettier-vs-extension
+# Json Prettier Extension for Visual Studio
 Plain and simple, straight to the point JSON formatter, not restricted to only ".json" file formats.
+
+```sh
+Edit > Prettify Json
+```
